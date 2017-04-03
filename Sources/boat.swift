9c81c05e8c31fc894d97e1046a0332d9421a848c
EmoBurrito <smith7351@saskpolytech.ca>
2017-04-03 16:51:31 -0700

    Board is resized

--- Sources/boat.swift
+++ Sources/boat.swift
@@ -3,7 +3,7 @@ public class Boat
 	//Symbol represenation
 	//Reeeeally wishing abstract classes were a thing
 	class var symbol: String {return " "}
-	class var length: Int {return 0}
+	class var length: Int {return 0} //Hardcoded because the arrays need to be non-static, but our generic populate call wants it to be static
 
 	//Should boats know their location, as well as if they've been hit or not?
 	// We should also be able to write a function so the boat knows how many times
@@ -13,7 +13,7 @@ public class Boat
 public class Sub : Boat
 {
 	override class var symbol: String { return "Ө" }
-	override class var length: Int {return 3} //TODO Why don't I just return the length of the array?
+	override class var length: Int {return 3}
 
 	var hits = [false, false, false] //Yet to be used
 }
@@ -21,7 +21,7 @@ public class Sub : Boat
 public class Carrier : Boat
 {
 	override class var symbol: String { return "Ш" }
-	override class var length: Int {return 4}
+	override class var length: Int {return 5}
 
 	var hits = [false, false, false, false]
 }
